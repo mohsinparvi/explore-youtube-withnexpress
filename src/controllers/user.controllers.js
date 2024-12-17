1ff4846470fc8193ddcfe37a783df0c824566755
Mohsin Hassan <86055307+mohsinparvi@users.noreply.github.com>
2024-12-17 15:04:43 +0500

    add cloudinary for image uploads; implement multer middleware for file handling and create user registration route

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,6 @@
+import { ApiResponse } from "../utils/api-response.js";
+import { asyncHandler } from "../utils/async-handler.js";
+
+const registerUser = asyncHandler(async (req, res) => {});
+
+export { registerUser };
