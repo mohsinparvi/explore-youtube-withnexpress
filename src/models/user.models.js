2412008e3e01874c1eadedd00c8d5e8f2a80b524
Mohsin Hassan <86055307+mohsinparvi@users.noreply.github.com>
2024-12-17 14:07:01 +0500

    add bcrypt and jsonwebtoken for password hashing and token generation; implement aggregate pagination in Comment and Video models

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,5 +1,6 @@
 import mongoose, { Schema } from "mongoose";
-
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 const userSchema = new Schema(
   {
     username: {
@@ -47,5 +48,28 @@ const userSchema = new Schema(
   },
   { timestamps: true }
 );
+userSchema.pre("save", async function (next) {
+  if (!this.modified("password")) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+
+  next();
+});
+userSchema.methods.isPasswordCorrect = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
+userSchema.methods.generateAccessToken = async function () {
+  //short live access token
+  jwt.sign(
+    {
+      _id: this._id,
+      username: this.username,
+      email: this.email,
+      fullname: this.fullname,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    { expiresIn:  process.env.ACCESS_TOKEN_EXPIRY }
+  );
+};
 
 export const User = mongoose.model("User", userSchema);
